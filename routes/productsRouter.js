ba1cd2c6bbd35ac5df717e9713f797e8031d64f5
Miguel Vásquez <angelzizu06@gmail.com>
2021-10-19 15:33:04 -0500

    ADD-> Middleware Validation (jio, validatorHandler)

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ProductsService = require("../services/productsService");
+const validatorHandler = require("../middlewares/validatorHandler");
+const {
+  getProductSchema,
+  updateProductSchema,
+  createProductSchema,
+} = require("../schemas/productSchema");
 
 const service = new ProductsService();
 
@@ -9,42 +15,55 @@ router.get("/", async (req, res) => {
   res.json(products);
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await service.findOne(id);
-    console.log(product);
-    res.json(product);
-  } catch (error) {
-    next(error);
+router.get(
+  "/:id",
+  validatorHandler(getProductSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      console.log(product);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.post("/", async (req, res) => {
-  const body = req.body;
-  const dataNewProduct = await service.create(body);
-  res.status(201).json({
-    message: "Created",
-    data: dataNewProduct,
-  });
-});
-
-router.put("/:id", async (req, res, next) => {
-  try {
-    const {
-      body,
-      params: { id },
-    } = req;
-    const productUpdated = await service.update(body, id);
-    res.json({
-      message: "Update with PUT",
-      data: productUpdated,
-      id,
+router.post(
+  "/",
+  validatorHandler(createProductSchema, "body"),
+  async (req, res) => {
+    const body = req.body;
+    const dataNewProduct = await service.create(body);
+    res.status(201).json({
+      message: "Created",
+      data: dataNewProduct,
     });
-  } catch (error) {
-    next(error);
   }
-});
+);
+
+router.put(
+  "/:id",
+  validatorHandler(getProductSchema, "params"),
+  validatorHandler(updateProductSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const {
+        body,
+        params: { id },
+      } = req;
+      const productUpdated = await service.update(body, id);
+      res.json({
+        message: "Update with PUT",
+        data: productUpdated,
+        id,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.patch("/:id", (req, res) => {
   const {
---
+++ schemas/productSchema.js
@@ -0,0 +1,29 @@
+const joi = require("joi");
+
+const id = joi.string().uuid();
+const name = joi.string().min(3).max(15);
+const price = joi.number().integer().min(10);
+const image = joi.string().uri();
+
+const createProductSchema = joi.object({
+  id: id,
+  name: name.required(),
+  price: price.required(),
+  image: image.required(),
+});
+
+const updateProductSchema = joi.object({
+  name: name,
+  price: price,
+  image: image,
+});
+
+const getProductSchema = joi.object({
+  id: id.required(),
+});
+
+module.exports = {
+  createProductSchema,
+  getProductSchema,
+  updateProductSchema,
+};
