2337a5d6bfd7682bd611f87d22807d240a505a2a
Miguel Vásquez <angelzizu06@gmail.com>
2021-10-20 10:34:07 -0500

    ADD-> Deploy with Heroku CLI

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ const {
   boomErrorHandler,
 } = require("./middlewares/errorHandler");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const routerAPI = require("./routes");
 
 app.use(express.json());
-app.use(cors()); // habilitar cualquier dominio
+
+const whiteList = ["http://localhost:8080", "https://quarzizus.web.app"];
+const options = {
+  origin: (origin, cb) => {
+    if (whiteList.includes(origin) || !origin) {
+      // error, allow
+      cb(null, true);
+    } else {
+      cb(new Error("No permitido"));
+    }
+  },
+};
+
+app.use(cors(options)); // habilitar cualquier dominio
 
 app.get("/", (req, res) => {
   res.send("HOME");
